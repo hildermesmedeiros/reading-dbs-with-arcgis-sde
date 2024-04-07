89cc66f0baa7b10d753ea6e5e4607178a330c295
Hildermes José Medeiros Filho <hildermes@gmail.com>
2024-04-07 13:57:40 -0300

    Update spatialdataframe.py

--- postgresql/spatialdataframe.py
+++ postgresql/spatialdataframe.py
@@ -4,51 +4,69 @@ import pandas as pd
 from arcgis.geometry import Geometry
 from psycopg2 import OperationalError
 from sqlalchemy.orm import aliased
+from sqlalchemy import or_, and_
+
 import arcpy
 from arcgis.features import GeoAccessor
+from typing import Union
 
 from dbcon import GetConn
 from models import GeometriaPolyModel
+from geom import CreateGeometry
 
 Base = declarative_base()
 
-SIRGAS_2000_WKID = 4674
 
-def geometria_poly_to_geoaccessor() -> GeoAccessor:
-    geometria_poly_alias = aliased(GeometriaPolyModel, name='GeometriaPolyModel')
-    try:
-        with GetConn() as (session, engine):
-            query = session.query(geometria_poly_alias.objectid,
-                                  geometria_poly_alias.campotexto,
-                                  geometria_poly_alias.campointeirocurto,
-                                  geometria_poly_alias.campointeirolongo,
-                                  geometria_poly_alias.campofloat,
-                                  geometria_poly_alias.campodata,
-                                  geometria_poly_alias.campobigint,
-                                  geometria_poly_alias.campodateonly,
-                                  geometria_poly_alias.campotimeonly,
-                                  geometria_poly_alias.campoblob,
-                                  geometria_poly_alias.globalid,
-                                  geometria_poly_alias.created_user,
-                                  geometria_poly_alias.created_date,
-                                  geometria_poly_alias.last_edited_user,
-                                  geometria_poly_alias.last_edited_date,
-                                  geometria_poly_alias.shape_wkt
-                                  )
-            sdf = pd.read_sql(query.statement, engine)
-            sdf['SHAPE'] = sdf['SHAPE'].map(
-                lambda shape: arcpy.FromWKT(shape).JSON
-            ).map(
-                lambda shape: Geometry(
-                    shape.replace('"wkid":null', f'"wkid":{SIRGAS_2000_WKID}')
+class QueryPolygon:
+    @staticmethod
+    def execute(query_all: bool = True) -> Union[pd.DataFrame, GeoAccessor]:
+
+        model_alias = aliased(GeometriaPolyModel, name='GeometriaPolyModel')
+        try:
+            with GetConn() as (session, engine):
+
+                query = session.query(model_alias.objectid,
+                                      model_alias.campotexto,
+                                      model_alias.campointeirocurto,
+                                      model_alias.campointeirolongo,
+                                      model_alias.campofloat,
+                                      model_alias.campodata,
+                                      model_alias.campobigint,
+                                      model_alias.campodateonly,
+                                      model_alias.campotimeonly,
+                                      model_alias.campoblob,
+                                      model_alias.globalid,
+                                      model_alias.created_user,
+                                      model_alias.created_date,
+                                      model_alias.last_edited_user,
+                                      model_alias.last_edited_date,
+                                      model_alias.shape_wkt
+                                      )
+
+                if not query_all:
+                    #query exemple
+                    query = query.filter(
+                        or_(
+                            and_(
+                                model_alias.campotexto != 'abc',
+                                model_alias.objectid != 5
+                            ),
+                            model_alias.globalid.is_(None)
+                        )
+                    )
+
+                sdf = pd.read_sql(query.statement, engine)
+                if sdf.empty:
+                    return sdf
+                sdf['SHAPE'] = sdf['SHAPE'].map(
+                    lambda shape: CreateGeometry.create_polygon(shape)
                 )
-            )
+                return sdf
+        except OperationalError as e:
+            print(e)
+        except Exception as e:
+            print(e)
 
-            return GeoAccessor.from_df(sdf, geometry_column='SHAPE')
-    except OperationalError as e:
-        print(e)
-    except Exception as e:
-        print(e)
 
 if __name__ == "__main__":
-    sdf = geometria_poly_to_geoaccessor()
+    sdf = QueryPolygon.execute()
